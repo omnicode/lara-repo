b87946615aac473a45255737aa19f12d00e7df02
davitbek <davitzeynalyan1991@gmail.com>
2017-09-23 23:53:39 +0400

    fix withcount for laravel 5.3

--- src/Builder/LaraQueryBuilder.php
+++ src/Builder/LaraQueryBuilder.php
@@ -1,63 +1,89 @@
 <?php
 namespace LaraRepo\Builder;
 
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Str;
 
-class LaraQueryBuilder extends Builder
+class LaraEloquentBuilder extends Builder
 {
-    /***
-     * @param array|mixed $column
-     * @return $this
-     */
-    public function addSelect($column)
-    {
-        if (!is_array($column)) {
-            $column = [$column];
-        }
-        foreach ($column  as $index =>  $col) {
-            if (is_array($this->columns) && in_array($col, $this->columns)) {
-                unset($column[$index]);
-            }
-        }
-        parent::addSelect($column);
-        return $this;
-    }
-
     /**
-     * @param array|\Closure|string $column
-     * @param null $operator
-     * @param null $value
-     * @param string $boolean
+     * @param mixed $relations
      * @return $this
-     * @throws \Exception
      */
-    public function where($column, $operator = null, $value = null, $boolean = 'and')
+    public function withCount($relations)
     {
-        $parentCall = true;
-        if (!empty($this->wheres)) {
-            foreach ($this->wheres as $where) {
-                if ($where['type'] != 'basic') {
-                    continue;
-                }
+        if (empty($relations)) {
+            return $this;
+        }
 
-                if($column == $where['column'] && $operator == $where['operator'] && $boolean == $where['boolean']) {
-                    if ($value == $where['value']) {
-                        $parentCall = false;
-                        break;
-                    }
+        if (is_null($this->query->columns)) {
+            $this->query->select([$this->query->from.'.' . $this->getModel()->getKeyName()]);
+        }
 
-                    if ($boolean == 'and') {
-                        throw new \Exception('In where query the ' . $column . ' cann\'t the same time have 2 values old = '
-                            . $where['value'] . ', new ' . $value . '');
-                    }
-                }
+        $relations = is_array($relations) ? $relations : func_get_args();
+
+        foreach ($this->parseWithRelations($relations) as $name => $constraints) {
+            // First we will determine if the name has been aliased using an "as" clause on the name
+            // and if it has we will extract the actual relationship name and the desired name of
+            // the resulting column. This allows multiple counts on the same relationship name.
+            $segments = explode(' ', $name);
+
+            unset($alias);
+
+            if (count($segments) == 3 && Str::lower($segments[1]) == 'as') {
+                list($name, $alias) = [$segments[0], $segments[2]];
             }
+
+            if (in_array('getHasRelationQuery', get_class_methods($this))) {
+                // for laravel 5.3
+                $relation = $this->getHasRelationQuery($name);
+            } elseif (in_array('getRelationWithoutConstraints', get_class_methods($this))) {
+                // for laravel 5.4
+                $relation = $this->getRelationWithoutConstraints($name);
+            } else {
+                throw new \Exception('fix it for new laravel version');
+            }
+
+            // Here we will get the relationship count query and prepare to add it to the main query
+            // as a sub-select. First, we'll get the "has" query and use that to get the relation
+            // count query. We will normalize the relation name then append _count as the name.
+
+            if (in_array('getRelationCountQuery', get_class_methods($relation))) {
+                //for laravel 5.3
+                $query = $relation->getRelationCountQuery(
+                    $relation->getRelated()->newQuery(), $this
+                );
+            } elseif (in_array('getRelationExistenceCountQuery', get_class_methods($relation))) {
+                //for laravel 5.4
+                $query = $relation->getRelationExistenceCountQuery(
+                    $relation->getRelated()->newQuery(), $this
+                );
+            } else {
+                throw new \Exception('fix it for new laravel version');
+            }
+
+
+            $query->callScope($constraints);
+
+            if (in_array('mergeModelDefinedRelationConstraints', get_class_methods($query))) {
+                //for laravel 5.3
+                $query->mergeModelDefinedRelationConstraints($relation->getQuery());
+            } elseif (in_array('mergeConstraintsFrom', get_class_methods($query))) {
+                //for laravel 5.4
+                $query->mergeConstraintsFrom($relation->getQuery());
+            } else {
+                throw new \Exception('fix it for new laravel version');
+            }
+
+            // Finally we will add the proper result column alias to the query and run the subselect
+            // statement against the query builder. Then we will return the builder instance back
+            // to the developer for further constraint chaining that needs to take place on it.
+            $column = snake_case(isset($alias) ? $alias : $name).'_count';
+
+            $this->selectSub($query->toBase(), $column);
         }
-        if ($parentCall) {
-            parent::where(...func_get_args());
-        }
+
         return $this;
     }
-
 
 }
