c7fe2fa32b902dcc521aeec4b097dc81633ba900
davitbek <davitzeynalyan1991@gmail.com>
2017-09-24 20:01:42 +0400

    improvement

--- src/Builder/LaraEloquentBuilder.php
+++ src/Builder/LaraEloquentBuilder.php
@@ -34,18 +34,46 @@ class LaraEloquentBuilder extends Builder
                 list($name, $alias) = [$segments[0], $segments[2]];
             }
 
-            $relation = $this->getRelationWithoutConstraints($name);
+            if (in_array('getHasRelationQuery', get_class_methods($this))) {
+                // for laravel 5.3
+                $relation = $this->getHasRelationQuery($name);
+            } elseif (in_array('getRelationWithoutConstraints', get_class_methods($this))) {
+                // for laravel 5.4
+                $relation = $this->getRelationWithoutConstraints($name);
+            } else {
+                throw new \Exception('fix it for new laravel version');
+            }
 
             // Here we will get the relationship count query and prepare to add it to the main query
             // as a sub-select. First, we'll get the "has" query and use that to get the relation
             // count query. We will normalize the relation name then append _count as the name.
-            $query = $relation->getRelationExistenceCountQuery(
-                $relation->getRelated()->newQuery(), $this
-            );
+
+            if (in_array('getRelationCountQuery', get_class_methods($relation))) {
+                //for laravel 5.3
+                $query = $relation->getRelationCountQuery(
+                    $relation->getRelated()->newQuery(), $this
+                );
+            } elseif (in_array('getRelationExistenceCountQuery', get_class_methods($relation))) {
+                //for laravel 5.4
+                $query = $relation->getRelationExistenceCountQuery(
+                    $relation->getRelated()->newQuery(), $this
+                );
+            } else {
+                throw new \Exception('fix it for new laravel version');
+            }
+
 
             $query->callScope($constraints);
 
-            $query->mergeConstraintsFrom($relation->getQuery());
+            if (in_array('mergeModelDefinedRelationConstraints', get_class_methods($query))) {
+                //for laravel 5.3
+                $query->mergeModelDefinedRelationConstraints($relation->getQuery());
+            } elseif (in_array('mergeConstraintsFrom', get_class_methods($query))) {
+                //for laravel 5.4
+                $query->mergeConstraintsFrom($relation->getQuery());
+            } else {
+                throw new \Exception('fix it for new laravel version');
+            }
 
             // Finally we will add the proper result column alias to the query and run the subselect
             // statement against the query builder. Then we will return the builder instance back
